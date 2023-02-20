31dda5c48d8e31dd14f97695138dc2ff40aaa9a9
Allen Mwiza <mwizaln2@yahoo.com>
2023-02-20 17:56:14 +0200

    first comit

---
+++ index.js
@@ -0,0 +1,13 @@
+let student=Bolatito;
+let classgroup= ArtsStudent;
+
+if(student= ArtsStudent) {
+    console.log("Arts subject");
+} else {
+    console.log("Not Arts subject");
+}
+
+let artssubjects=Government, Economics, Literature, History;
+if (classgroup = artssubjects) {
+    console.log("Member")
+}
